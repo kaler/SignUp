4ee146873bdae4d25a1b82251e9e9276a92d9c19
Parveen Kaler <pk@smartfulstudios.com>
2010-07-24 21:48:26 -0700

    Initial Commit

---
+++ Classes/SignupAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  SignupAppDelegate.h
+//  Signup
+//
+//  Created by Parveen Kaler on 10-07-24.
+//  Copyright Smartful Studios Inc 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignupAppDelegate : NSObject <UIApplicationDelegate> {
+  UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Signup
+//
+//  Created by Parveen Kaler on 10-07-24.
+//  Copyright Smartful Studios Inc 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
